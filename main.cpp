c486de11299cc0ec56d951e962252bfc0e766edb
Tomasz Drzewiecki <kn65op@gmail.com>
2011-09-12 14:00:04 +0200

    stworzenie projektu w NB i konfiguracja

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: tomko
+ *
+ * Created on 12 wrzesień 2011, 13:56
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
+
