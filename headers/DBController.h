b15f9d5323804f3a4cb162eae06ea81c08eeaeb6
kn65op <kn65op@gmail.com>
2011-11-16 22:52:13 +0100

    Dodanie kontrolera do bazy

---
+++ headers/DBController.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   DBController.h
+ * Author: tomko
+ *
+ * Created on 16 listopad 2011, 22:51
+ */
+
+#ifndef DBCONTROLLER_H
+#define	DBCONTROLLER_H
+
+class DBController
+{
+public:
+  DBController();
+  DBController(const DBController& orig);
+  virtual ~DBController();
+private:
+
+};
+
+#endif	/* DBCONTROLLER_H */
+
---
+++ src/DBController.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   DBController.cpp
+ * Author: tomko
+ * 
+ * Created on 16 listopad 2011, 22:51
+ */
+
+#include "../headers/DBController.h"
+
+using namespace std;
+
+DBController::DBController()
+{
+}
+
+DBController::DBController(const DBController& orig)
+{
+}
+
+DBController::~DBController()
+{
+}
+
