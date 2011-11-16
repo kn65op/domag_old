8c3cb6986802a42dfcaeca788f7a03fc73096ef1
kn65op <kn65op@gmail.com>
2011-11-16 22:50:41 +0100

    Dodanie klasy pośredniczącej między sqlite3.c a kontrolelem do danej bazy

---
+++ headers/SQLite3Controller.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   SQLite3Controller.h
+ * Author: tomko
+ *
+ * Created on 16 listopad 2011, 22:49
+ */
+
+#ifndef SQLITE3CONTROLLER_H
+#define	SQLITE3CONTROLLER_H
+
+class SQLite3Controller
+{
+public:
+  SQLite3Controller();
+  SQLite3Controller(const SQLite3Controller& orig);
+  virtual ~SQLite3Controller();
+private:
+
+};
+
+#endif	/* SQLITE3CONTROLLER_H */
+
---
+++ src/SQLite3Controller.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   SQLite3Controller.cpp
+ * Author: tomko
+ * 
+ * Created on 16 listopad 2011, 22:49
+ */
+
+#include "../headers/SQLite3Controller.h"
+
+using namespace std;
+
+SQLite3Controller::SQLite3Controller()
+{
+}
+
+SQLite3Controller::SQLite3Controller(const SQLite3Controller& orig)
+{
+}
+
+SQLite3Controller::~SQLite3Controller()
+{
+}
+
