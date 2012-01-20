fed8192fdec53a7d20148e4b3342fc9f10e7d53c
Tomasz Drzewiecki <kn65op@gmail.com>
2012-01-20 16:52:08 +0100

    Dodanie nowych tabel

--- src/DBController.cpp
+++ src/DBController.cpp
@@ -23,11 +23,14 @@ bool DBController::checkTables()
 {
   try
   {
-    string check_articles = "select id, name, group_id from articles;";
+    string check_articles = "select article_id, name, group_id from articles;";
     sql.executeSelectQuery(check_articles);
     sql.clearSelectQuery();
-    string check_groups = "select id, name from groups;";
+    string check_groups = "select group_id, name from groups;";
     sql.executeSelectQuery(check_groups);
+    sql.clearSelectQuery();
+    string check_users = "select user_id, name from users;";
+    sql.executeSelectQuery(check_users);
     sql.clearSelectQuery();
   } catch (SQLException sql)
   {
@@ -43,17 +46,23 @@ bool DBController::createTables()
     
     string create_groups = "create table groups"
             "("
-            " id integer primary key autoincrement,"
+            " group_id integer primary key autoincrement,"
             " name text not null"
             ");";
     sql.executeQuery(create_groups);
     string create_articles = "create table articles"
             "("
-            " id integer primary key autoincrement,"
+            " article_id integer primary key autoincrement,"
             " name text not null,"
             " group_id not null references gorups(id)"
             ");";
     sql.executeQuery(create_articles);
+    string create_users = "create table users"
+            "("
+            " user_id integer primary key autoincrement,"
+            " name text not null"
+            ");";
+    sql.executeQuery(create_users);
   } catch (SQLException)
   {
     return false;
@@ -69,6 +78,8 @@ bool DBController::dropTables()
     sql.executeQuery(drop_articles);
     string drop_groups = "drop table if exists groups;";
     sql.executeQuery(drop_groups);
+    string drop_users = "drop table if exists users;";
+    sql.executeQuery(drop_users);
   } catch (SQLException)
   {
     return false;
